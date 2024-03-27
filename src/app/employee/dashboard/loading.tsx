6f20ff14b164bd361cc044a40652ffd5b5fc6d5e
Puneet Dwivedi <puneetdwivedi@igtechso.com>
2024-03-27 15:37:46 +0530

    changes in caleder-show-details

---
+++ src/app/employee/dashboard/loading.tsx
@@ -0,0 +1,14 @@
+import Loader from "@/app/components/Loader";
+import React from "react";
+
+type Props = {};
+
+const loading = (props: Props) => {
+  return (
+    <div>
+      <Loader />
+    </div>
+  );
+};
+
+export default loading;
--- src/app/employee/dashboard/slot-management/calender/__components/CalenderShowDetail.tsx
+++ src/app/employee/dashboard/slot-management/calender/__components/CalenderShowDetail.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import { Divider, Switch, Typography } from "antd";
+import { Button, Divider, Switch, Typography } from "antd";
 import { CiCalendarDate } from "react-icons/ci";
-import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
-
+import { CheckOutlined, CloseOutlined, EditOutlined } from "@ant-design/icons";
 import React from "react";
 import { TCalender } from "@/app/types/calender";
 import DescriptionItem from "@/app/components/DescriptionItem.tsx";
+import { calender_status } from "../__utils.ts/constant";
 const { Title, Text } = Typography;
 
 type Props = {
   activeCalender: TCalender;
 };
-
 
 const CalenderShowDetail = (props: Props) => {
   return (
@@ -38,19 +37,45 @@ const CalenderShowDetail = (props: Props) => {
           />
         </div>
       </div>
-      <div>
-        <Title level={5}>Slot Details</Title>
-
-        <div>
-          <ul>
-            <li>
-              <div>
-                <DescriptionItem title="Start Time" content="Thu Mar 21 2024"/>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Divider />
+      {props.activeCalender &&
+        props.activeCalender.status === calender_status.open && (
+          <div>
+            <Title level={5}>Slot Details</Title>
+            <div>
+              <ul className="list-disc">
+                {props.activeCalender.slots.map((slot, index) => {
+                  return (
+                    <li key={slot._id}>
+                      <div className="grid grid-cols-2 gap-x-2 items-end">
+                        <DescriptionItem
+                          title="Start Time"
+                          content={new Date(
+                            slot.start_time
+                          ).toLocaleTimeString()}
+                        />
+                        <DescriptionItem
+                          title="End Time"
+                          content={new Date(slot.end_time).toLocaleTimeString()}
+                        />
+                        <DescriptionItem
+                          title="Slot Limit"
+                          content={slot.slot_limit}
+                        />
+                        <div>
+                          <button className="p-2 border bg-gray-50 hover:bg-gray-100 rounded-full w-10 h-10">
+                            <EditOutlined />
+                          </button>
+                        </div>
+                      </div>
+                      <Divider />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        )}
     </div>
   );
 };
