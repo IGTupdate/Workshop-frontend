dc501e51444f0107b45293acfd9ce31997f52269
shubham98270 <shubhamrajput@igtechso.com>
2024-04-24 16:18:07 +0530

    add loader on notification

--- src/app/dashboard/notifications/page.tsx
+++ src/app/dashboard/notifications/page.tsx
@@ -1,42 +1,50 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import StepBar from './__components/StepBar';
-import Notifications from './__components/Notifications';
-import { useAppSelector } from '@/app/store/reduxHooks';
-import { appointmentNotification } from '@/app/services/operations/notification/appointment';
-import { getCustomerInitData } from '@/app/services/operations/appointment/appointment';
-
+import React, { useEffect, useState } from "react";
+import StepBar from "./__components/StepBar";
+import Notifications from "./__components/Notifications";
+import { useAppSelector } from "@/app/store/reduxHooks";
+import { appointmentNotification } from "@/app/services/operations/notification/appointment";
+import { getCustomerInitData } from "@/app/services/operations/appointment/appointment";
+import Loader from "@/app/components/Loader";
 
 const page = () => {
+  const [notificationData, setNotificationData] = useState({});
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const [notificationData, setNotificationData] = useState({});
+  const customerId = useAppSelector((state) => state.auth.authData._id);
 
-    const customerId = useAppSelector((state) => state.auth.authData._id);
+  const initData = async () => {
+    try {
+      if (!customerId) return;
+      setLoading(true);
+      const initAppointmentData = await getCustomerInitData(customerId);
+      const initNotificationData = await appointmentNotification(
+        initAppointmentData._id
+      );
+      setNotificationData(initNotificationData);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+    }
+  };
 
-    const initData = async () => {
-        try {
-            if (!customerId) return;
-            const initAppointmentData = await getCustomerInitData(customerId);
-            const initNotificationData = await appointmentNotification(initAppointmentData._id);
-            setNotificationData(initNotificationData);
-        } catch (err) {
-        }
-    };
+  useEffect(() => {
+    initData();
+  }, [customerId]);
 
-    useEffect(() => {
-        initData();
-    }, [customerId]);
+  return (
+    <div className="h-screen pt-20 pb-32 px-4 md:py-0 overflow-auto">
+      {/* step bar */}
+      <StepBar />
 
-
-    return (
-        <div className='h-screen pt-20 pb-32 px-4 md:py-0 overflow-auto'>
-            {/* step bar */}
-            <StepBar />
-
-            {/* notifications */}
-            <Notifications show={"All"} notificationData={notificationData} />
-        </div>
-    );
+      {/* notifications */}
+      {loading ? (
+        <Loader />
+      ) : (
+        <Notifications show={"All"} notificationData={notificationData} />
+      )}
+    </div>
+  );
 };
 
 export default page;
